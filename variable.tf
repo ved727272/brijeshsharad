809f254ed71c948fbdceb88b898cbdddb9b1d773
ved727272 <brijesh.zte@gmail.com>
2025-07-13 19:01:08 +0530

    codeadded

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "dev_resource_group" {
+    type = map(any)
+}
+
+variable "dev_virtual_network" {
+     type = map(any)
+}
+
+variable "dev_subnet" {
+     type = map(any)
+}
+
+variable "dev_public_ip" {
+     type = map(any)
+
+}
+variable "dev_vm_configs" {
+     type = map(any)
+}
+
+
