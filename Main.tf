54adc84d241dfd9d1fa8486ede9244f32207fb9f
ved727272 <brijesh.zte@gmail.com>
2025-07-15 18:36:07 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,32 +1,4 @@
-module "resource_group" {
-    source = "../../Module/Azurerm_Resource_group"
-    resource_group = var.dev_resource_group
-  
+resource "azurerm_resource_group" "rg" {
+    name = "raman-rg"
+    location = "eastus"
 }
-
-module "virtual_network" {
-    source = "../../Module/Azurerm_VNET"
-    depends_on = [module.resource_group]
-    virtual_network = var.dev_virtual_network
-}
-
-module "subnet" {
-    source = "../../Module/Azurerm_Subnet"
-    depends_on = [module.virtual_network]
-    subnet = var.dev_subnet
-}
-
-module "public_ip" {
-    source = "../../Module/Azurerm_Public_ip"
-    depends_on = [module.resource_group]
-    public_ip = var.dev_public_ip
-}
-
-module "virtual_machine" {
-    source = "../../Module/Azurerm_VM"
-    depends_on = [module.subnet, module.public_ip ]
-    vm_configs = var.dev_vm_configs
-   
-}
-
-
